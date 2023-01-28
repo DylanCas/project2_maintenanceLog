990a5559ae14238b51c6bfebc2a2947905641264
JacobYaws <Jacob.Yaws@du.edu>
2023-01-27 20:25:09 -0700

    Updating this branch to be up to speed with Trong's branch

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -12,7 +12,7 @@ router.get('/', withAuth, async (req, res) => {
     const users = userData.map((project) => project.get({ plain: true }));
 
     res.render('homepage', {
-      users,
+       users,
 
       logged_in: req.session.logged_in,
     });
--- seeds/index.js
+++ seeds/index.js
@@ -6,12 +6,11 @@ const userData = require('./users.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  process.exit(0);
+  await User.create({
+    name: '',
+    email: '',
+    password: '',
+});
 };
 
 seedDatabase();
