e497bd49e957bcf3b6bb47341508440db31521c9
DylanCas <djcasabona12@gmail.com>
2023-02-01 22:36:44 -0700

    main.handlebars was emptied. That is our first landing page, cannot be blank. Corrected axios import statement

--- controllers/api/carmdRoutes.js
+++ controllers/api/carmdRoutes.js
@@ -1,6 +1,5 @@
 const { response } = require("express");
-const axios = require('axios/dist/browser/axios.cjs'); // browser commonJS bundle (ES2017)
-// const axios = require('axios/dist/node/axios.cjs'); // node commonJS bundle (ES2017)
+const axios = require('axios');
 
 const apiUrl = "http://api.carmd.com/v3.0";
 require("dotenv").config();
@@ -8,20 +7,7 @@ require("dotenv").config();
 const router = require("express").Router();
 
 router.get("/vin/:vin", async (req, res) => {
-    const url = `${apiUrl}/decode?vin=${req.params.vin}`;
     
-    // ***axios npm will be used in place of following***
-    // https://www.npmjs.com/package/axios#axios-api
-//   const response = await fetch(url, {
-//     method: "get",
-//     headers: {
-//       "partner-token": process.env.CARMDTOKEN,
-//       "Authorization": process.env.CARMDKEY,
-//       "Content-Type": "application/json",
-//     },
-//   });
-  console.log(response)
-  res.status(200).json(response);
 });
 
 module.exports = router;
