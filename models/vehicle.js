4d6a02ff247abb13442360a0825fe34442122caa
DylanCas <djcasabona12@gmail.com>
2023-02-02 19:46:03 -0700

    idk, conflict issue

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,54 +1,41 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Vehicle extends Model {}
+class Vehicle extends Model {
+
+}
 
 Vehicle.init(
     {
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: "user",
-                key: "id"
-            }
-        },
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true, 
-        },
-        make: {
-            type: DataTypes.STRING,
-            allowNull: false,
+            autoIncrement: true,
         },
         model: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        year: {
-            // may alter/need to alter, to STRING
-            type: DataTypes.INTEGER,
+        trim: {
+            type: DataTypes.STRING,
             allowNull: false,
         },
         engine: {
             type: DataTypes.STRING,
-            allowNull: true,
+            allowNull: false,
         },
-        mileage: {
-            // may alter/need to alter, to STRING
+        milage: {
             type: DataTypes.INTEGER,
             allowNull: true,
         }
     },
     {
         sequelize,
-        timestamps: false,
-        // Prevent sequelize from renaming the table
+        timestamps: false, 
+        modelName: 'vehicle',
         freezeTableName: true,
-        modelName: 'vehicle'
-      }
+    }
 );
 
 module.exports = Vehicle;
