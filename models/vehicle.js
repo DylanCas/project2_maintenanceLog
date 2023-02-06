59269688089702b97b5ed2adc27caf80ca728393
Scott StJohn <scottst.89@gmail.com>
2023-02-05 21:10:37 -0700

    fixed code for vehicle.js on my end

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 class Vehicle extends Model {}
 
@@ -46,47 +46,13 @@ Vehicle.init(
             allowNull: true,
         }
     },
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    make: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    model: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    year: {
-      // may alter/need to alter, to STRING
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    engine: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    mileage: {
-      // may alter/need to alter, to STRING
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    // vin: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: true,
-    // },
-    // add user id
-  },
-  {
-    sequelize,
-    timestamps: false,
-    // Prevent sequelize from renaming the table
-    freezeTableName: true,
-    modelName: "vehicle",
-  }
+    {
+        sequelize,
+        timestamps: false,
+        // Prevent sequelize from renaming the table
+        freezeTableName: true,
+        modelName: 'vehicle'
+      }
 );
 
 module.exports = Vehicle;
